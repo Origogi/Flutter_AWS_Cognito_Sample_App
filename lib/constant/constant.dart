bf7b1c0577785c488efc5c1de3d0561c3673926c
Jeongtae Kim <rlawjdxo1212@naver.com>
2020-07-04 14:36:20 +0900

    rename file

---
+++ lib/constant/constant.dart
@@ -0,0 +1,9 @@
+// Setup AWS User Pool Id & Client Id settings here:
+const awsUserPoolId = 'us-west-2_MDsgG3C6u';
+const awsClientId = '36p9qdev3t9gp210c3mfuaq3dk';
+
+const identityPoolId = 'us-west-2:01875e1e-c020-482f-b789-9628fb1c6f28';
+
+// Setup endpoints here:
+const region = 'us-west-2';
+const endpoint = 'https://coazikyeong.auth.us-west-2.amazoncognito.com';
